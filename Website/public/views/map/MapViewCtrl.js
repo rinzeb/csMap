0f8c600ed81bfba8b9e7025bc44d77bdb6e4e869
Erik Vullings <erik.vullings@gmail.com>
2014-11-04 07:47:18 +0100

    Partial upload

---
+++ Website/public/views/map/MapViewCtrl.js
@@ -0,0 +1,21 @@
+﻿var MapView;
+(function (MapView) {
+    var MapViewCtrl = (function () {
+        // dependencies are injected via AngularJS $injector
+        // controller's name is registered in Application.ts and specified from ng-controller attribute in index.html
+        function MapViewCtrl($scope, $mapService) {
+            this.$scope = $scope;
+            this.$mapService = $mapService;
+            // 'vm' stands for 'view model'. We're adding a reference to the controller to the scope
+            // for its methods to be accessible from view / HTML
+            $scope.vm = this;
+        }
+        MapViewCtrl.$inject = [
+            '$scope',
+            'mapService'
+        ];
+        return MapViewCtrl;
+    })();
+    MapView.MapViewCtrl = MapViewCtrl;
+})(MapView || (MapView = {}));
+//# sourceMappingURL=MapViewCtrl.js.map
